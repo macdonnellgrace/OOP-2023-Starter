499c9c4d26af665a2c2fc85b2de9f612b493cb07
Grace <gracerosevalentine@gmail.com>
2023-04-26 16:25:51 +0100

    building word class

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -1,5 +1,23 @@
 package ie.tudublin;
 
 public class Word {
+
+    private String word;
+    // follows class
+
+
+    public String getWord() {
+        return word;
+    }
+
+    // constructor methods
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    // string
+    public String toString() {
+        return "Word ="  + " follows = " + word;
+    }
     
 }
